81e702169f79465954aba76d705b0aee54a9e281
Onchari <onchariwycliffe@gmail.com>
2019-01-23 19:26:27 +0000

    Add project files.

---
+++ eChrome/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eChrome.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
